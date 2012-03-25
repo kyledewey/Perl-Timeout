26ba54c2df6d5fb971bf923358a07c8cf06f13b0
Kyle Dewey <kyle.t.dewey@gmail.com>
2012-03-24 20:34:36 -0400

    Replaced the hacky and flaky kill (negative pid)... with the new killFamily function.  Still being given a stress test, but it works in basic tests.

--- timeout.pl
+++ timeout.pl
@@ -3,17 +3,85 @@
 # perl version of timeout
 # I've found that timeout doesn't work quite right
 # noninteractively, so I wrote this
+# ps must be installed
 #
 # timeouts are in minutes
-
+#
 use strict;
+use constant SECONDS_IN_MINUTE => 60;
+
+# returns a reference to a mapping of pids to parent pids
+sub pidToParentPid() {
+    my @output = `ps ax -o pid,ppid`;
+    my %retval;
+
+    foreach my $line ( @output ) {
+	if ( $line =~ /(\d+)\s+(\d+)/ ) {
+	    $retval{ $1 } = $2;
+	}
+    }
+
+    return \%retval;
+}
+
+# returns a mapping of parent pids to direct children
+# the direct children are references to arrays
+sub parentPidToDirectChildren() {
+    my $hashRef = pidToParentPid();
+    my %retval;
+
+    foreach my $childPid ( keys( %$hashRef ) ) {
+	my $arrRef;
+	if ( exists( $retval{ $hashRef->{ $childPid } } ) ) {
+	    $arrRef = $retval{ $hashRef->{ $childPid } };
+	} else {
+	    my @arr;
+	    $arrRef = \@arr;
+	    $retval{ $hashRef->{ $childPid } } = $arrRef;
+	}
+	push( @$arrRef, $childPid );
+    }
+
+    return \%retval;
+}
+
+# Because Perl doesn't use inner functions properly...
+# Takes:
+# -pid
+# -Mapping of pids to direct children
+# returns an array of pids
+sub recurChildProcesses( $$ ) {
+    my ( $pid,
+	 $directChildrenMap ) = @_;
+    my @retval;
+    my $childrenRef = $directChildrenMap->{ $pid };
+    foreach my $child ( @$childrenRef ) {
+	@retval = ( @retval, recurChildProcesses( $child, $directChildrenMap ) );
+    }
+    @retval = ( @retval, @$childrenRef );
+    return @retval;
+}
+
+# given a pid, returns the pids of all child processes
+# they are returned in order such that the deepest children are returned first
+sub childProcesses( $ ) {
+    my $pid = shift();
+    my $hashRef = parentPidToDirectChildren();
+    return recurChildProcesses( $pid, $hashRef );
+}
+
+# given  kill signal number and a pid, kills everything
+# in the family, deepest children first
+sub killFamily( $$ ) {
+    my ( $signal, $pid ) = @_;
+    kill $signal, childProcesses( $pid ), $pid;
+}
 
 my $duration = shift();
 if ( $duration !~ /^\d+$/ ) {
     die "Duration must be an integer specifying the number of minutes to wait";
 }
 
-my $majorPid = getppid();
 my $commandPid = fork();
 if ( !defined( $commandPid ) ) {
     die "Failed to fork 1";
@@ -32,19 +100,10 @@ if ( !defined( $commandPid ) ) {
 	exit( 0 );
     } else { # ( $watchPid == 0 ) {
 	my $secondsWaited = 0;
-	while ( $secondsWaited < $duration * 60 ) {
-	    $secondsWaited += sleep( 60 );
+	while ( $secondsWaited < $duration * SECONDS_IN_MINUTE ) {
+	    $secondsWaited += sleep( SECONDS_IN_MINUTE );
 	}
 
-	# UGLY HACK
-	# ideally, I'd like to do:
-	# kill 2 $commandPid, which will kill only the command process
-	# however, I've found that this doesn't always kill subprocesses
-	# (sbt being a major offender). I'm still not certain why 
-	# kill -2, $commandPid doesn't work either (sbt in mind)
-	kill -2, $majorPid;
-
-	# this would be needed if the above line were more precise
-	#kill 2, getppid();
+	killFamily( 2, $commandPid );
     }
 }
